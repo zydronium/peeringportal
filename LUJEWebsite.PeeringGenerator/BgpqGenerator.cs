9d250fdcc13588e77f30896976aba24c7bae0458
Jelle Luteijn <git@jelle.luteijn.email>
2024-10-19 23:31:08 +0200

    run parallel for ipv4 and ipv6

--- LUJEWebsite.PeeringGenerator/BgpqGenerator.cs
+++ LUJEWebsite.PeeringGenerator/BgpqGenerator.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using NpgsqlTypes;
+using LUJEWebsite.PeeringGenerator.Models;
 
 namespace LUJEWebsite.PeeringGenerator
 {
@@ -230,37 +231,37 @@ namespace LUJEWebsite.PeeringGenerator
 		}
 
 		public void GenerateFilters()
-        {
-            // BIRD IPv4 and IPv6
-            foreach (var v in new[] { 4, 6 })
-            {
-                if (as_set == "ANY")
-                {
-                    continue;
-                }
+		{
+			if (as_set == "ANY")
+			{
+				return;
+			}
 
-                string filename = $"{Configuration.RoutefiltersLocation}/AS{asnumber}.prefixset.bird.ipv{v}";
+			// BIRD IPv4 and IPv6
+			Parallel.ForEach(new[] { 4, 6 }, v =>
+			{
+				string filename = $"{Configuration.RoutefiltersLocation}/AS{asnumber}.prefixset.bird.ipv{v}";
 
-                if (File.Exists(filename))
-                {
-                    if (DateTime.Now - File.GetLastWriteTime(filename) > TimeSpan.FromMinutes(5))
+				if (File.Exists(filename))
+				{
+					if (DateTime.Now - File.GetLastWriteTime(filename) > TimeSpan.FromMinutes(5))
 					{
 						RunBgpq4(filename, v, as_set, "b", null, "");
 						//parseSaveFilter(filename, v);
 						Console.WriteLine($"bird ipv{v} refreshed: {filename}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"bird ipv{v} cached: {filename}");
-                    }
-                }
-                else
-                {
-                    RunBgpq4(filename, v, as_set, "b", null, "");
+					}
+					else
+					{
+						Console.WriteLine($"bird ipv{v} cached: {filename}");
+					}
+				}
+				else
+				{
+					RunBgpq4(filename, v, as_set, "b", null, "");
 					//parseSaveFilter(filename, v);
 					Console.WriteLine($"bird ipv{v} created: {filename}");
-                }
-            }
+				}
+			});
         }
     }
 }
